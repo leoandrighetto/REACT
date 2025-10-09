cd210a5b64f53e73b5726239a2a6a1ace41728b2
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-10-08 22:06:41 -0300

    Add files via upload

---
+++ aula3/Questao.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+/*
+
+export default function Questao (props) {
+    const pergunta = props.pergunta;
+    const resposta = props.resposta;
+
+  }
+
+*/
+
+/*
+export default function Questao ({pergunta,resposta}) {
+
+    return <div>
+        <h2>P e r g u n t a </h2>
+        <button> S I M </button> <button> N Ã O </button>
+    </div>
+
+}
+*/
+
+export default function Questao ({pergunta,resposta}) {
+
+    let retorno = useState(0);
+
+    function Verificar(resposta){
+        if (resposta == resposta){
+            alert("Acertou!")
+            retorno[1]("Acertou");
+        }    else{
+                alert("Errou")
+                retorno[1]('Errou');
+            }
+        }
+
+    // const clicarSim = () => Verificar("sim");
+    // const clicarNao = () => Verificar("não");    
+        
+    return <div>
+        <h2>P e r g u n t a: {pergunta}</h2>
+        <button onClick = {() => Verificar("sim")}> S I M </button> <button onClick={() => Verificar("não")}> N Ã O </button>
+    </div>
+
+}
---
+++ aula3/Questionario.jsx
@@ -0,0 +1,22 @@
+import Questao from "./Questao";
+
+export default function Questionario() {
+
+    return <>
+
+        <Questao
+            pergunta="A Terra é Plana?"       <--------propriedade
+            resposta="não" />
+
+        <Questao
+            pergunta="Um quilometro tem 500 metros?"
+            resposta="não" />
+
+        <Questao
+            pergunta="O pai ta on?"
+            resposta="sim" />
+
+
+    </>
+
+}
