c4fa7a7099e402a1063311a9e17d12c07e851d4a
Leonardo Andrighetto <leoandrighetto02@gmail.com>
2025-10-31 21:18:34 -0300

    Add files via upload

---
+++ Cofre/src/App.jsx
@@ -0,0 +1,15 @@
+import Cofre from "./Cofre"
+
+function App() {
+
+
+  return (
+    <>
+
+    <Cofre senha="123"/>
+
+    </>
+  )
+}
+
+export default App
---
+++ Cofre/src/Cofre.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import Tecla from "./Tecla"
+
+export default function Cofre({senha}){
+    
+    const senhaNumerica = senha.split("").map(n => Number(n)); 
+
+    const [mensagem,setMensagem ]= useState('');
+    const [ tentativas, setTentativas] = useState(3);
+
+    const [Tecla1,setTecla1] = useState(0);
+    const [Tecla2,setTecla2] = useState(0);
+    const [Tecla3,setTecla3] = useState(0);
+
+    const tecla1 = (contador) => {setTecla1(contador)}
+    const tecla2 = (contador) => {setTecla2(contador)}
+    const tecla3 = (contador) => {setTecla3(contador)}
+    // console.log(senhaNumerica);
+
+    const senhaDigitada = [Tecla1,Tecla2,Tecla3];
+
+    const abrirCofre = ()=>{
+
+      let T = tentativas;
+
+      if (T>0){
+
+        const comparar = senhaNumerica.filter((e,i) => e === senhaDigitada[i]);
+        if(comparar.length === senhaNumerica.length){
+          setMensagem('Cofre Aberto!')
+        }else{
+          setTentativas(T-1);
+          setMensagem('Errou a Senha. Tentativas: ' + T)
+        }
+      }else{
+        setMensagem('Errou a Senha. Tentativas: ' + T)
+      }  
+    }
+    console.log(tentativas)
+    return (
+        <>
+        <div style={{display:'flex',justifyContent:'center'}}>
+
+        <Tecla onGuardar={tecla1}/>
+        <Tecla onGuardar={tecla2}/>
+        <Tecla onGuardar={tecla3}/>
+
+        <button onClick={()=>abrirCofre()}>Abrir</button>
+        </div>
+        <div style={{textAlign:'center',padding:'10px'}}>
+          Tentativas: {tentativas} <br />
+          {mensagem}
+        </div>
+        </>
+  )
+
+
+}
